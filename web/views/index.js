2515193938e91ea3e8c1e9b5e6a1f4525ec2fdf7
Mr.Zhy <LiZhenShuai@handsome.com>
2018-11-05 19:51:36 +0800

    完成前台页面对英雄的增删改功能

--- web/views/index.js
+++ web/views/index.js
@@ -33,31 +33,57 @@ $(function(){
         id = $(this).data('id')
         getHero()
     })
+    // 编辑英雄函数
     function getHero(){
         $.ajax({
             url:'http://127.0.0.1:5000/getHero/' + id,
             type:'get',
             success:function(res){
-                var html = template('getHero',res) 
+                var html = template('getHero',res.data[0]) 
                 $('.ui.modal').html(html)
             }
         })
     }
+    // 点击添加英雄显示弹出层
+    $('.addHero').on('click',function(){
+        $('.ui.modal').modal('show')
+        var html = template('getHero',{}) 
+       $('.ui.modal').html(html)
+   })
     // 修改提交
     $('.ui.modal').on('click','.button',function(){
          var id = $(this).data('id')
+        // console.log(id);
          var isdel = $('#numbers').val()
-         var name = $('[name=name]').val()
-         var gender = $('[name=gender]').val()
-         var say = $('[name=say]').val()   
+         var name = $('[name=name]').val().trim()
+         var gender = $('[name=gender]').val().trim()
+         var say = $('[name=say]').val().trim()
+         var url = null; 
+        if(!name) {
+            alert('请输入英雄姓名')
+            return
+        }
+        if(!gender) {
+            alert('请选择英雄性别')
+            return
+        }
+        if(!say) {
+            alert('请输入英雄台词')
+            return
+        }
         if(isdel == '2'){
             alert('请选择英雄状态')
             return
         }
+        if(id){
+            url = 'http://127.0.0.1:5000/updateHero/' + id;
+        }else {
+            url = 'http://127.0.0.1:5000/addHero'
+        }
         $('.ui.modal').modal('hide') 
         function updateHero(){
             $.ajax({
-                url:'http://127.0.0.1:5000/updateHero/' + id,
+                url:url,
                 type:'post',
                 data:{
                     name:name,
@@ -71,5 +97,6 @@ $(function(){
             })
         }
         updateHero()
-    })   
+    }) 
+    
 })
